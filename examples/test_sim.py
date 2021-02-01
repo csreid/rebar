Lots, but mostly just fix softmax implementation

examples/test_sim.py
@@ -17,27 +17,28 @@ env.observation_space.high = -mins
 agt = ADP(
 	env.action_space,
 	env.observation_space,
-	bins=9,
+	bins=5,
 	initial_temp=2000,
 	gamma=0.99
 )
 
 s = env.reset()
-
-for step in range(5000):
-	a = int(agt.get_action(s))
+for _ in range(50000):
+	a = agt.get_action(s)
 	sp, r, done, _ = env.step(a)
 
-	s_d = agt._convert_to_discrete(s)
 	agt.handle_transition(s, a, r, sp, done)
-
 	s = sp
 	if done:
+		done = False
 		s = env.reset()
 
 s = env.reset()
-print(f's: {s}')
-print(f'sims:')
-agt.simulate(s, 0)
-sp, r, done, _ = env.step(0)
-print(f'Actual: {sp}')
+discrete_state = agt._convert_to_discrete(s)
+taken_action = np.sum(agt.F[discrete_state][0][sp] for sp in agt.F[discrete_state][0])
+print(s)
+print(agt.continuize_state(s))
+print(f'Seen the state {agt.visits[agt._convert_to_discrete(s)]} times; taken the action {taken_action}')
+print(np.mean(agt.simulate_step(s, 0, n=50), axis=0))
+s, r, done, _ = env.step(a)
+print(s)

rebar/learners/adp.py
@@ -139,10 +139,11 @@ class ADP(Learner):
 		return new_obs
 
 	def sample_state(self, s, n):
+		# Take in a discretized state and return `n` random states in that "bin"
+
 		if not all(type(i) == int for i in s):
 			s = self._convert_to_discrete(s)
 
-		# Take in a discretized state and return `n` random states in that "bin"
 		left_edge = np.array(s) - 1
 		right_edge = np.array(s)
 
@@ -154,6 +155,41 @@ class ADP(Learner):
 
 		return gen
 
+	def simulate_step(self, s, a, n=1):
+		if not all(type(i) == int for i in s):
+			s = self._convert_to_discrete(s)
+
+		sp_s = []
+		r_s = []
+		p_s = []
+		for sp in self.F[s][a]:
+			p_sp, r = self.p_sp(s, a, sp)
+			p_s.append(p_sp)
+			r_s.append(r)
+			sp_s.append(self.continuize_state(sp))
+
+		ns = np.random.choice(len(sp_s), p=np.array(p_s), size=n)
+		ns = np.array([
+			sp_s[i]
+			for i
+			in ns
+		])
+
+		return ns
+
+	def continuize_state(self, s):
+		# Take a discretized state and return the middle continuous state value
+		if not all(type(i) == int for i in s):
+			s = self._convert_to_discrete(s)
+
+		left_edge = np.array(s) - 1
+		right_edge = np.array(s)
+
+		low = np.array([self.bounds[idx][l] for idx, l in enumerate(left_edge)])
+		high = np.array([self.bounds[idx][h] for idx, h in enumerate(right_edge)])
+
+		return (low + high) / 2
+
 	def action_probabilities(self, s, temp=None):
 		if temp is None:
 			temp = self._temp
@@ -161,8 +197,7 @@ class ADP(Learner):
 		s = self._convert_to_discrete(s)
 
 		qs = self.get_action_vals(s)
-		qs = qs - np.max(qs)
-		ps = [(e ** (q / temp)) / np.sum(e ** (qs / temp)) for q in qs]
+		ps = softmax(qs / temp)
 
 		return ps
 

rebar/learners/qlearner.py
@@ -142,7 +142,7 @@ class QLearner(Learner):
 		return a
 
 	def exploitation_policy(self, s):
-		eps = 0.05
+		eps = 0.
 		if np.random.random() > eps:
 			best_action = torch.argmax(self.Q(s[None, :])).detach().numpy()
 			a = best_action